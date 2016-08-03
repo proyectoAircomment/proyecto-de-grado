60e77f19f079c15ff65d9017bc36da390c5100e0
proyectoaircomment <proyecto.aircomment@gmail.com>
2016-08-03 20:00:40 +0000

    Se implementa el metodo assocArrayPosRpt en repot_decoder.php que formatea los mensajes de posicion

--- main.php
+++ main.php
@@ -16,16 +16,21 @@
     
     
     $rd = new ReportDecoder();
+    
     echo 'Contenido (formateado) del mensaje: <hr>';
+    
     echo $rd->getContenido();
+    
     echo '<hr> Array que contiene el mensaje METAR desglosado: <hr>';
     $rd->setArray();
+    var_dump($rd->getArrayPosRpt());
     
-    echo '<hr>Metars: <hr>';
-    foreach($rd->getAeropuertosDeReporte() as $city)
+    echo json_encode($rd->getArrayPosRpt());
+    /*echo '<hr>Metars: <hr>';
+    foreach($rd->getAeropuertosDeReporteMetar() as $city)
     {
         echo $crawler->conocerMetarDe($city).'<br>';
-    }
+    }*/
     
     
     //http://190.27.249.245/
@@ -57,6 +62,34 @@ DT DDL MTR 032259 M50A
 /TYP 1/STA SKRG/STA SKBO';
 
 echo strlen($metar);*/
+
+/*
+el resultado debe ser algo como:
+
+{
+   "messageHeader":"3N01 RPT",
+   "flightNumber":"8138",
+   "flightDay":"23",
+   "origin":"SKBO",
+   "destination":"SKBQ",
+   "aircraftId":"HK-4818",
+   "UTCtime":"195906",
+   "position":{
+      "lat":[
+         "N",
+         "09216"
+      ],
+      "lon":[
+         "W",
+         "074273"
+      ]
+   },
+   "altitude":"+35133",
+   "speed":"749",
+   "fuel":"00475"
+}
+
+*/
 
     
 
--- sms/report_decoder.php
+++ sms/report_decoder.php
@@ -3,7 +3,7 @@
     {
         private $mensaje, $tipo;
         private $cabecera,$contenido;
-        private $arrayTipoMetar,$assocArrayMetar;
+        private $arrayTipoMetar,$assocArrayMetar,$assocArrayPosRpt;
         
         public function __construct($mensaje = 'QU ANPOCFC
 
@@ -15,9 +15,9 @@
                                 
                                 DT DDL MTR 032259 M50A
                                 
-                                - 001 WXRQ 8149/03 SKBO/SKRG HK-4811
-                                
-                                /TYP 1/STA SKRG/STA SKBO')
+                                - 3N01 POSRPT 8138/23 SKBO/SKBQ HK-4818
+        
+        /UTC 195906/POS N09216W074273/ALT +35133/MCH 749/FOB 00475')
         {
             $this->mensaje = $mensaje;
             $this->cabecera = '';
@@ -54,13 +54,24 @@
             return $this->tipo;
         }
         
+        //crea uno o varios arrays dependiendo del tipo de reporte
+        //emitido de la aeronave hacia el sistema
         function setArray()
         {
             $arr = preg_split("/[\s,]+/",$this->contenido);
+            //la posición 1 del vetor contiene el tipo de reporte
             switch ($arr[1]) 
             {
                 case 'WXRQ':
                     $this->setArrayMetar();
+                    break;
+                    
+                case 'POSRPT':
+                    $this->setArrayPosRpt();
+                    break;
+                
+                case 'OUTRP' || 'OFFRP' || 'ONRP' || 'INRP':
+                    $this->setArrayOOOI();
                     break;
                 
                 default:
@@ -70,10 +81,14 @@
             
         }
         
+        //Fragmenta el contenido del mensaje
+        //y lo convierte en un array asociativo
         function setArrayMetar()
         {
+            //abstrae las ciudades de las que se solicita el reporte
             $citiesReport = preg_split("/[\s,]+/",trim(str_replace('STA','',strstr($this->contenido,'STA'))));
             $arr = preg_split("/[\s,]+/",$this->contenido);
+            //asigna los valores del array asociativo
             $this->assocArrayMetar = array('messageHeader'=>$arr[0].' '.$arr[1],
                                      'flightNumber' =>$arr[2],
                                      'flightDay'    =>$arr[3],
@@ -85,25 +100,68 @@
             var_dump($this->assocArrayMetar);
         }
         
-        //falta implementarlo correctamente
-        function getAeropuertosDeReporte()
+        //Fragmenta el contenido del mensaje
+        //y lo convierte en un array asociativo
+        function setArrayPosRpt()
+        {
+            //crea un vector de palabras que serán eliminadas en el mensaje
+            $replace = array('UTC','POS','ALT','MCH','FOB');
+            $arr = preg_split("/[\s,]+/",trim(str_replace($replace,'',$this->contenido)));
+            //separación de los valores de posición recibidos como un string continuo (sin espacios)
+            $toSplit = preg_split("/(,?\s+)|((?<=[a-z])(?=\d))|((?<=\d)(?=[a-z]))/i",$arr[8]);
+            //formateo del valor de las coordenadas en g° m' s"
+            $dirLat = $toSplit[0];
+            $segsLat = substr($toSplit[1],-1,1);
+            $minsLat = substr($toSplit[1],-3,2);
+            $degsLat = substr($toSplit[1],0,-3);
+            $dirLon = $toSplit[2];
+            $segsLon = substr($toSplit[3],-1,1);
+            $minsLon = substr($toSplit[3],-3,2);
+            $degsLon = substr($toSplit[3],0,-3);
+            
+            //asignación de los valores de orientación a la posición como un array
+            $position = array('lat' => array('direction'=>$dirLat, 'dms'=>array('d'=>$degsLat,'m'=>$minsLat,'s'=>$segsLat)), 
+                              'lon' => array('direction'=>$dirLon, 'dms'=>array('d'=>$degsLon,'m'=>$minsLon,'s'=>$segsLon)));
+            //asigna los valores del array asociativo
+            $this->assocArrayPosRpt = array(
+                                     'messageHeader'=>$arr[0].' '.$arr[1],
+                                     'flightNumber' =>$arr[2],
+                                     'flightDay'    =>$arr[3],
+                                     'origin'       =>$arr[4],
+                                     'destination'  =>$arr[5],
+                                     'aircraftId'   =>$arr[6],
+                                     'UTCtime'      =>$arr[7],
+                                     'position'     =>$position,
+                                     'altitude'     =>$arr[9],
+                                     'speed'        =>$arr[10],
+                                     'fuel'         =>$arr[11]);
+        }
+        
+        //crea los array asociativos OOOI
+        //por cada tipo de reporte asigna un formato distinto
+        function setArrayOOOI()
+        {
+            //implementarlo
+        }
+        
+        //
+        function getArrayMetar()
+        {
+            return $this->assocArrayMetar;
+        }
+        
+        function getArrayPosRpt()
+        {
+            return $this->assocArrayPosRpt;
+        }
+        
+        //del array de solicitudes Metar creado
+        //se retorna las ciudades de las cuales se quiere
+        //conocer las condiciones meteorológicas.
+        function getAeropuertosDeReporteMetar()
         {
             return $this->assocArrayMetar['citiesReport'];
         }
-        
-        function administrarReportes($tipo)
-        {
-            
-        }
-        
-        function sacarCiudadesdeEminsionDeAcuerdoAlTipo()
-        {
-            //falta implementarlo
-        }
-        
-        
-        
-        
         
     }
 ?>
